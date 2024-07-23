9f89a0d7cd356477b6f5a80b8d4fb915ed26cc7c
Rohan Maiti <maitirohan2019@gmail.com>
2024-07-23 12:19:14 +0530

    class of Events

---
+++ 23-07/js/script.js
@@ -0,0 +1,15 @@
+let myPara = document.querySelector("#mypara")
+// myPara.setAttribute("class","x");
+
+// console.log(myPara.classList);
+myPara.classList.add("x");
+// myPara.classList.remove("y");
+// myPara.classList.toggle("x");
+// console.log(myPara);
+
+
+// event driven programing 
+
+function test(){
+    console.log("Button clicked");
+}
